2d07ec91eca5375f84a1c009c861fe07c8e1f469
LidorHefets <khsurj3018@gmail.com>
2025-05-05 16:36:00 +0300

    עדכון מס' 5 - תפריט המבורגר

--- services/global.js
+++ services/global.js
@@ -24,4 +24,18 @@
         toggleBtn.textContent = "🌙";
       }
     });
+
+
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.getElementById("navMenu");
+
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+
+  
+    const isExpanded = navMenu.classList.contains("show");
+    hamburger.setAttribute("aria-expanded", isExpanded);
   });
+  });
+
+ 
