1e03f564ea988f1e7565566fedf2f0c8eb099895
LidorHefets <khsurj3018@gmail.com>
2025-05-06 13:04:08 +0300

    עדכון מס' 6 - רספונסיביות

--- services/global.js
+++ services/global.js
@@ -4,7 +4,6 @@
   document.addEventListener("DOMContentLoaded", function () {
     const toggleBtn = document.getElementById("darkModeToggle");
   
-    // בעת טעינת העמוד – לבדוק אם היה מצב כהה
     if (localStorage.getItem("darkMode") === "enabled") {
       document.body.classList.add("dark-mode");
       toggleBtn.textContent = "☀️";
@@ -15,7 +14,6 @@
     toggleBtn.addEventListener("click", () => {
       document.body.classList.toggle("dark-mode");
   
-      // שמירת מצב בלוקל סטורג'
       if (document.body.classList.contains("dark-mode")) {
         localStorage.setItem("darkMode", "enabled");
         toggleBtn.textContent = "☀️";
